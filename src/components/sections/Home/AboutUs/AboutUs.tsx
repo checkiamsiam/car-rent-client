9a3bcc335604759690ba09d72876bf9fbdd56696
Naimur Rahman <naimur2231@gmail.com>
2024-03-22 20:12:11 +0600

    about us section added

--- src/components/sections/Home/AboutUs/AboutUs.tsx
+++ src/components/sections/Home/AboutUs/AboutUs.tsx
@@ -4,30 +4,28 @@ import { FaShoppingBasket } from "react-icons/fa";
 
 const AboutUs = () => {
   return (
-    <div className="row justify-content-between align-items-center px-1 ps-4">
-      <div className="col-lg-4 col-md-4">
-        <h2 style={{ color: "#43121d" }} className="fs-1 fw-bold">
-          Good food choices
-          <br />
-          are good
-          <br />
-          investments.
-        </h2>
-        <p className="mt-3 mb-4">
-          Becoming a cook is my dream since I was little, and here
-          <br />I have found a way to make it happen.
-        </p>
-        <div className="mt-2 mb-3">
-          <Button className="border-0 me-3" style={{ background: "#a82d49" }}>
-            Order Now <FaShoppingBasket />
-          </Button>
-          <Button type="primary">Learn More</Button>
+    <div className="md:flex items-center justify-center w-full md:h-[60vh] h-full mb-16">
+      <div className="w-full md:w-1/2 bg-[#ee945c] md:h-[60vh] h-full ">
+        <div className="md:mx-20 mx-7 mt-8 md:mt-24 py-4 md:py-0">
+          <h2 style={{ color: "#43121d" }} className="text-3xl font-semibold">
+            What Do You Know About Us?
+          </h2>
+          <p className="mt-3 mb-4">
+            SHR Car Rental: Your trusted travel companion. We provide
+            top-quality vehicles and exceptional service to make your journey
+            memorable. With a commitment to excellence and customer
+            satisfaction, we ensure a seamless rent experience. Explore with
+            confidence of SHR Car Rental.
+          </p>
+          <div className="mt-4 mb-3">
+            <Button type="primary">Learn More</Button>
+          </div>
         </div>
       </div>
-      <div className="col-lg-8 col-md-8">
+      <div className="w-full md:w-1/2">
         <img
-          className="img-fluid w-100"
-          src="https://img.freepik.com/free-photo/vegan-almond-milk-chia-seeds-pudding-with-banana-kiwi_2829-5952.jpg"
+          className="md:h-[60vh] h-full  w-full"
+          src="https://i.ibb.co/jv13LMN/istockphoto-1208725993-612x612.jpg"
           alt=""
         />
       </div>
