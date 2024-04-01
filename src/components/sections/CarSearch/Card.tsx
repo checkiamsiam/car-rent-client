cdeebf788d18826c1d4e82c12e1d799329aeb409
checkiamsiam <issiam02415@gmail.com>
2024-04-01 17:58:08 +0600

    Add showInfo state and toggle functionality

--- src/components/sections/CarSearch/Card.tsx
+++ src/components/sections/CarSearch/Card.tsx
@@ -4,16 +4,18 @@ import { ICar } from "@/types/ApiResponse";
 import { Button } from "antd";
 import dayjs from "dayjs";
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { BsFillFuelPumpFill } from "react-icons/bs";
 import { FaDoorClosed } from "react-icons/fa";
-import { IoLogoXbox } from "react-icons/io";
-import { IoBagOutline } from "react-icons/io5";
+import { IoIosArrowDown, IoIosArrowUp, IoLogoXbox } from "react-icons/io";
+import { IoBagOutline, IoCheckmarkOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
 import { PiCassetteTapeThin } from "react-icons/pi";
 import { TbAirConditioning, TbManualGearbox } from "react-icons/tb";
 
 const Card = ({ car }: { car: ICar }) => {
+  const [showInfo, setShowInfo] = useState(false);
   const router = useRouter();
   const params = useSearchParams();
   const searchQuery = Object.fromEntries(params.entries());
@@ -103,27 +105,70 @@ const Card = ({ car }: { car: ICar }) => {
       </div>
       {/* card fooder */}
       <hr className="my-3 border-[1px]" />
-      <div className="flex justify-between flex-wrap">
-        <div className="flex gap-2">
-          <button className="bg-[green] text-white py-1 px-2 rounded-md">Enropcar</button>
-          <button className="bg-[#2C6EA7] text-white py-1 px-2 rounded-md">8.1</button>
-          <div>
-            <h1 className="font-semibold">Very good</h1>
-            <h3 className="text-xs">200+ reviews</h3>
+      <div>
+        <div className="flex justify-between items-center flex-wrap">
+          <div className="flex gap-2">
+            <button className="bg-[green] text-white py-1 px-2 rounded-md">Enropcar</button>
+            <button className="bg-[#2C6EA7] text-white py-1 px-2 rounded-md">8.1</button>
+            <div>
+              <h1 className="font-semibold">Very good</h1>
+              <h3 className="text-xs">200+ reviews</h3>
+            </div>
+          </div>
+          <div className="flex gap-3 justify-center items-center">
+            <h1 className="text-[#2C6EA7] flex gap-1 justify-center items-center cursor-pointer" onClick={() => setShowInfo(!showInfo)}>
+              {" "}
+              <AiOutlineInfoCircle color="#2C6EA7" size={25} />
+              Important info
+              {showInfo ? <IoIosArrowUp color="#2C6EA7" size={25} /> : <IoIosArrowDown color="#2C6EA7" size={25} />}
+            </h1>
+            <h1 className="text-[#2C6EA7] flex gap-1">
+              <MdOutlineEmail color="#2C6EA7" size={25} />
+              Email quote
+            </h1>
           </div>
         </div>
-        <div></div>
-        <div className="flex gap-3">
-          <h1 className="text-[#2C6EA7] flex gap-1">
-            {" "}
-            <AiOutlineInfoCircle color="#2C6EA7" size={25} />
-            Important info
-          </h1>
-          <h1 className="text-[#2C6EA7] flex gap-1">
-            <MdOutlineEmail color="#2C6EA7" size={25} />
-            Email quote
-          </h1>
-        </div>
+        {showInfo && (
+          <div className="mt-5 grid md:grid-cols-2 grid-cols-1">
+            <p className="flex items-center">
+              <span className="text-green-500 pr-2 text-xl">
+                <IoCheckmarkOutline />
+              </span>
+              Customer rating: 8.4 / 10
+            </p>
+            <p className="flex items-center">
+              {" "}
+              <span className="text-green-500 pr-2 text-xl py-1">
+                <IoCheckmarkOutline />
+              </span>
+              Most popular fuel policy
+            </p>
+            <p className="flex items-center">
+              <span className="text-green-500 pr-2 text-xl py-2">
+                <IoCheckmarkOutline />
+              </span>
+              Short queues
+            </p>
+            <p className="flex items-center">
+              <span className="text-green-500 pr-2 text-xl">
+                <IoCheckmarkOutline />
+              </span>
+              Easy to find counter
+            </p>
+            <p className="flex items-center">
+              <span className="text-green-500 pr-2 text-xl py-2">
+                <IoCheckmarkOutline />
+              </span>
+              Helpful counter staff
+            </p>
+            <p className="flex items-center">
+              <span className="text-green-500 pr-2 text-xl">
+                <IoCheckmarkOutline />
+              </span>
+              Well-maintained cars
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
